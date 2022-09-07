28785a1fbaefc213a12b5b322e23ae351c59aa5a
srvnk <105205717+srvnk@users.noreply.github.com>
2022-09-07 14:43:41 +0530

    Create SensorRead.cs

---
+++ SensorProcess/SensorRead.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SensorProcess
+{
+    public class SensorRead
+    {        
+        public void ReadSensorData()
+        {
+            Random randData = new Random();
+            for (int i = 0; i < 50; i++)
+            {
+                float sensorData = randData.Next(0, 100);
+                Console.WriteLine(sensorData);
+            }
+        }
+    }
+}
