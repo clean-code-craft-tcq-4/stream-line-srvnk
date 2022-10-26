1cf3abbbde1fc51f0baf635365f32305f6d7694a
srvnk <105205717+srvnk@users.noreply.github.com>
2022-10-26 11:52:42 +0530

    Update SensorRead.cs

--- SensorProcess/SensorRead.cs
+++ SensorProcess/SensorRead.cs
@@ -1,17 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 namespace SensorProcess
 {
     public class SensorRead
     {
-        public void ReadSensorData()
+        public List<float> GenerateTemperatureData(float max, float min)
         {
             Random randData = new Random();
+            List<float> temperatureData = new List<float>();
+
             for (int i = 0; i < 50; i++)
             {
-                float sensorData = randData.Next(0, 100);
-                Console.WriteLine(sensorData);
+                temperatureData.Add((float)(randData.NextDouble()*(max - min)) + min);
             }
+            return temperatureData;
+        }
+
+        public List<float> GenerateChargeRateData(float max, float min)
+        {
+            Random randData = new Random();
+            List<float> chargeRateData = new List<float>();
+            for (int i = 0; i < 50; i++)
+            {
+                chargeRateData.Add((float)(randData.NextDouble() * (max - min)) + min);
+            }
+            return chargeRateData;
         }
     }
 }
